5bcf8be2ab5bff231543b0079b77b956475a7da1
Alexander Titov <titov.alex627@gmail.com>
2021-05-08 20:47:46 +0300

    feat: added search block in home

--- src/screens/home-screen/HomeScreen.jsx
+++ src/screens/home-screen/HomeScreen.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { FontAwesome } from '@expo/vector-icons'
 
+import { THEME } from '../../constants'
 import { MenuButton } from '../../components/common'
 import { Map } from '../../components/map'
 
@@ -14,6 +16,20 @@ export const HomeScreen = () => {
     <View style={styles.wrapper}>
       <Map />
       <MenuButton onPress={openDrawer} />
+      <View style={styles.containerGeo}>
+        <View style={{ ...styles.blockSearch, ...styles.currentGeo }}>
+          <View style={styles.containerIcon}>
+            <FontAwesome name='map-marker' size={24} color={THEME.MINE_SHAFT} />
+          </View>
+          <Text>Текущий адрес</Text>
+        </View>
+        <View style={styles.blockSearch}>
+          <View style={styles.containerIcon}>
+            <FontAwesome name='search' size={24} color={THEME.MINE_SHAFT} />
+          </View>
+          <Text>Куда перевезти?</Text>
+        </View>
+      </View>
     </View>
   )
 }
